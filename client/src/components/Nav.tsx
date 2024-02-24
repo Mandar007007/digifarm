c5e0fb7d29f3571caa58cd2c7ab3359610beb418
Sureja-Rushi <rushisureja48@gmail.com>
2024-02-25 01:34:13 +0530

    Again make some changes in navbar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useDisclosure } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
@@ -39,7 +39,7 @@ const CustomNavLink: React.FC<CustomNavLinkProps> = ({
   );
 };
 
-export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
+export default function Nav({ toggleLogin, isLoggedIn, routes, user }: any) {
 
   const dispatch = useDispatch();
 
@@ -56,7 +56,7 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
 
   const logout = async () => {
 
-    try{
+    try {
       await axios.get("http://localhost:3000/api/v1//logout", {
         headers: {
           "Content-Type": "application/json",
@@ -69,7 +69,7 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
 
       toast.success("Logged out successfully");
       toggleLogin();
-    }catch(err){
+    } catch (err) {
       toast.error("Error");
     }
   };
@@ -83,18 +83,18 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <div className="flex items-center">
-            <img src="/logo.png" className="md:h-12 h-6" alt="vite" />
-            <p className="ml-4 text-xl text-green-300 font-semibold ">Digi<span className="text-slate-300">Farm</span></p>
+              <img src="/logo.png" className="md:h-12 h-6" alt="vite" />
+              <p className="ml-4 text-xl text-green-300 font-semibold ">Digi<span className="text-slate-300">Farm</span></p>
             </div>
           </CustomNavLink>
           <div className="flex md:order-2 space-x-3 md:space-x-0  rtl:space-x-reverse">
             {!isLoggedIn && (
-              <button 
-                type="button" 
+              <button
+                type="button"
                 className="text-white bg-gradient-to-br from-green-800 to-green-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium text-sm px-5 py-2.5 text-center"
-                onClick={onOpenLogin}  
+                onClick={onOpenLogin}
               >
-                
+
                 Login/Register
                 <Login
                   isOpen={isOpenLogin}
@@ -112,7 +112,7 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
             {isLoggedIn && (
               <Menu>
                 <MenuButton className="border-2 border-white-500 rounded-full">
-                  <Avatar name="Dan Abrahmov" src={"/avatar.jpeg"} size={"sm"}/>
+                  <Avatar name="Dan Abrahmov" src={"/avatar.jpeg"} size={"sm"} />
                 </MenuButton>
                 <MenuList bg={"gray.900"}>
                   <CustomNavLink
@@ -146,21 +146,21 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
                   _focus={{
                     bg: "gray.500",
                   }}
-                  
+
                 />
                 <MenuList bg={"gray.900"}>
                   {routes.map((route: any, index: any) => (
-                    <MenuItem key={index} bg={"gray.900"}>
-                      <CustomNavLink
-                        to={route.path}
-                        className=""
-                        activeClassName="text-green-500"
-                      >
-                        {route.name}
-                      </CustomNavLink>
-                    </MenuItem>
+                  <MenuItem key={index} bg={"gray.900"}>
+                    <CustomNavLink
+                      to={route.path}
+                      className=""
+                      activeClassName="text-green-500"
+                    >
+                      {route.name}
+                    </CustomNavLink>
+                  </MenuItem>
                   ))}
-                  
+
                 </MenuList>
               </Menu>
             </div>
@@ -170,18 +170,59 @@ export default function Nav({ toggleLogin, isLoggedIn , routes , user }: any) {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 m">
-              {routes.map((route: any, index: any) => (
-                <li key={index}>
-                  <CustomNavLink
-                    to={route.path}
-                    className="text-white hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50 "
-                    activeClassName="text-green-400 font-semibold "
-                  >
-                    {route.name}
-                  </CustomNavLink>
-                </li>
-              ))}
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+              <li>
+                <CustomNavLink
+                  to={routes[0].path}
+                  className="text-white hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50 "
+                  activeClassName="text-green-400 font-semibold "
+                >
+                  {routes[0].name}
+                </CustomNavLink>
+              </li>
+              <li>
+                <CustomNavLink
+                  to={routes[1].path}
+                  className="text-white hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50 "
+                  activeClassName="text-green-400 font-semibold "
+                >
+                  {routes[1].name}
+                </CustomNavLink>
+              </li>
+              <li>
+                <Menu>
+                  <MenuButton>ML Models</MenuButton>
+                  <MenuList bg={"gray.900"}>
+                    <MenuItem bg={"gray.900"}>
+                      <CustomNavLink
+                        to={routes[2].path}
+                        className="hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50"
+                        activeClassName="text-green-500"
+                      >
+                        {routes[2].name}
+                      </CustomNavLink>
+                    </MenuItem>
+                    <MenuItem bg={"gray.900"}>
+                      <CustomNavLink
+                        to={routes[3].path}
+                        className="hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50"
+                        activeClassName="text-green-500"
+                      >
+                        {routes[3].name}
+                      </CustomNavLink>
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </li>
+              <li>
+                <CustomNavLink
+                  to={routes[4].path}
+                  className="text-white hover:text-green-300 font-semibold hover:transition-colors ease-in-out delay-50 "
+                  activeClassName="text-green-400 font-semibold "
+                >
+                  {routes[4].name}
+                </CustomNavLink>
+              </li>
             </ul>
           </div>
         </div>
