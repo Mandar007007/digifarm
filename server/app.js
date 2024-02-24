60d6280639ad216565f5488692652b89cff0bfb9
Mandar007007 <teamm2d07@gmail.com>
2024-02-25 00:13:25 +0530

    io logic done

--- server/app.js
+++ server/app.js
@@ -19,18 +19,37 @@ app.use(cors({
 
   const server = http.createServer(app); 
 
- io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        // origin: "https://kisaan-sathi.vercel.app",
-        methods: ["GET", "POST"],
-    }
-});
-
-io.on('connection', (socket) => {
+  io.on('connection', (socket) => {
     socket.on('message-sent', (data) => {
         io.sockets.emit('message-received', { message: data.message });
     });
+
+    socket.on('placeBid',async (data) => {
+        
+        const currentAuction = await Auction.findById(data.auction._id)
+        // console.log(data);
+        
+            if(currentAuction) {
+            currentAuction.bidPrice += data.bidAmount;
+            }
+            currentAuction.bidder = data.bidder;
+            currentAuction.bidderEmail = data.bidderEmail;
+
+        await currentAuction.save();
+
+        
+        io.to(data.auction._id).emit('updateAuction',{updatedAuction: currentAuction,bidder:data.bidder,bidderEmail:data.bidderEmail})
+        }
+    )
+    socket.on('redirect',(data) => {
+        socket.join(data.auction._id)
+    })
+    socket.on('joinAuction', (data) => {
+        socket.join(data.auctionId)
+      });
+    socket.on('message-passed',(data) => {
+        io.to(data.auction_id).emit('message-to-all',{message:data.message})
+    })
 });
 
 //router
